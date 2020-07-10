11937df4b89c29ab3cbfabff2ea85f7ad13f535a
Zakaria <daud0009@d.umn.edu>
2020-07-09 20:59:16 -0500

    Finished 2nd function to calculate difference between dates

--- dates.py
+++ dates.py
@@ -1,6 +1,7 @@
+# https://codechalleng.es/bites/67/ coding challenge 
 from datetime import date, timedelta
 
-start_100days = date(2017, 3, 30)
+start_100days = date(2020, 7, 9)
 pybites_founded = date(2016, 12, 19)
 pycon_date = date(2018, 5, 8)
 
@@ -15,9 +16,9 @@ def get_hundred_days_end_date():
 def get_days_between_pb_start_first_joint_pycon():
     """Return the int number of days"""
     diff = abs(pycon_date - pybites_founded)
-    pass
+    print(diff)
+    #pass
 
 
-#get_hundred_days_end_date()
-get_days_between_pb_start_first_joint_pycon()
 get_hundred_days_end_date()
+get_days_between_pb_start_first_joint_pycon()
